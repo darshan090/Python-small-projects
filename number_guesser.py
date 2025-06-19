1d7e7e8d2a73a8aea90987773e8aa12626d618f3
Darshan <darshanjariwala090@gmail.com>
2025-06-19 18:14:06 +0530

    Changes to game

--- number_guesser.py
+++ number_guesser.py
@@ -1,13 +1,35 @@
 import random
 
-user = int(input("Enter a number between 1 to 20: "))
-print(f"You entered number {user}")
+top_of_range = input("Type a number: ")
 
-r = random.randrange(1,21)
-print(f"random number is {r}")
-
-
-if r == user:
-    print("You guessed the right number")
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+        print("Please type a number greater than 0")
+        quit()
 else:
-    print("Incorrect answer")
+    print("Please type a number next time")
+    quit()
+
+random_number = random.randint(0,top_of_range)
+print(random_number)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time")
+        continue
+    
+    if user_guess == random_number:
+        print("You've got it right")
+        break
+    else:
+        print("You've got it wrong")
+
+
+print("You got it in" , guesses , "guesses")
